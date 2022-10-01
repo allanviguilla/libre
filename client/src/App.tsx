6283eeefbc00de6ba6b8f58b433caceb875057f4
Kathryn Gao <kathryn.kuroko@gmail.com>
2022-10-01 15:29:42 -0700

    save

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react'
 import LoginPage from './components/LogIn/LoginPage'
 
+interface UserInterface {
+
+}
 
 const App = () => {
+  const [user, setUser] = useState(null)
   return (
     <div id="app">
       <h2>Libre</h2>
