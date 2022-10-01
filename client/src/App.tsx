6e9fc65531b6e76dd7b6a35cc48cecc5657da2ad
Nick <nicolastiennguyen@gmail.com>
2022-10-01 14:41:01 -0700

    feat: added sign in button with pop up modal

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,26 @@
 import React, { useState } from 'react'
+import { authentication } from '../../configs/config';
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const App = () => {
+
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(authentication, provider)
+    .then(res => {
+      console.log(res)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
   return (
     <div id="app">
       <h2>Libre</h2>
+      <button onClick={signInWithGoogle}>
+        Login
+      </button>
     </div>
   )
 }
