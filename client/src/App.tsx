6f168883cad930b28263e19163a780cee0464a91
Nick <nicolastiennguyen@gmail.com>
2022-10-01 17:48:38 -0700

    feat: able to create/update firestore db with data retrieved from google sign in

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,35 @@
 import React, { useState } from 'react'
-import { authentication } from '../../configs/config';
+import { authentication, db } from '../../configs/config';
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { collection, doc, setDoc, addDoc, getDoc } from "firebase/firestore";
 
 const App = () => {
 
   const signInWithGoogle = () => {
     const provider = new GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/calendar.events');
+    provider.addScope('https://www.googleapis.com/auth/calendar');
+    provider.addScope('https://www.googleapis.com/auth/calendar.settings.readonly');
+    provider.addScope('https://www.googleapis.com/auth/calendar.events.readonly');
+    provider.addScope('https://www.googleapis.com/auth/calendar.readonly');
     signInWithPopup(authentication, provider)
     .then(res => {
-      console.log(res)
+      const { displayName, email, photoUrl, oauthAccessToken, refreshToken } = res._tokenResponse;
+      getDoc(doc(db, "users", email))
+        .then((userData) => {
+          const friends = (userData.data() === undefined) ? [] : userData.data().friends;
+          setDoc(doc(db, "users", email), {
+            displayName: displayName,
+            email: email,
+            photoUrl: photoUrl,
+            oauthAccessToken: oauthAccessToken,
+            refreshToken: refreshToken,
+            friends: friends,
+          })
+        })
     })
     .catch(err => {
-      console.log(err)
+      console.log('error signing in: ', err)
     })
   }
 
