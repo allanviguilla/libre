fe2971eedddfbf726b0a2514640a4479568a1a3d
Nick <nicolastiennguyen@gmail.com>
2022-10-03 16:03:34 -0700

    feat: reordered interface, passed down setUser as a prop

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,8 +4,8 @@ import LoginPage from './components/LogIn/LoginPage'
 import { ChakraProvider } from '@chakra-ui/react';
 
 interface UserInterface {
-  email: string,
   displayName: string,
+  email: string,
   photoUrl: string,
   oauthAccessToken: string,
   refreshToken: string,
@@ -15,32 +15,14 @@ interface UserInterface {
 
 }
 
-const dummyUser = {
-  email: 'hackreactor@gmail.com',
-  displayName: 'eric do',
-  oauthAccessToken: 'insert access token here',
-  refreshToken: 'insert refresh token here',
-  friends: ['hepner.thomas2@gmail.com'],
-  bio: 'im better than julien',
-  profile: 'ericprofile.com'
-}
-
 const App = () => {
-
-  // TODO: set state of user after user-login
-
-  // to show calendar view:
-  // const [user, setUser] = useState<UserInterface | null>(dummyUser);
-
-  // to show user login view:
   const [user, setUser] = useState<UserInterface | null>(null);
 
-  // console.log(user);
   return (
     <ChakraProvider>
       <div id="app">
         {
-          user ?  <HomePage /> : <LoginPage />
+          user ?  <HomePage /> : <LoginPage setUser={setUser}/>
         }
       </div>
     </ChakraProvider>
