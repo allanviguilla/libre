df781b2c9f098a52c55699c0feee2bcaec7b4236
Nick <nicolastiennguyen@gmail.com>
2022-10-03 16:04:05 -0700

    feat: pulled down setUser from App and used to set state of user

--- client/src/components/LogIn/LoginPage.tsx
+++ client/src/components/LogIn/LoginPage.tsx
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GoogleButton from 'react-google-button';
 import { authentication, db } from '../../../../configs/config';
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { doc, setDoc, getDoc } from "firebase/firestore";
 
-const LoginPage = () => {
+const LoginPage = (props) => {
+  const { setUser } = props;
+
   const signInWithGoogle = () => {
     const provider = new GoogleAuthProvider();
     provider.addScope('https://www.googleapis.com/auth/calendar.events');
     provider.addScope('https://www.googleapis.com/auth/calendar');
-    provider.addScope('https://www.googleapis.com/auth/ƒcalendar.settings.readonly');
+    provider.addScope('https://www.googleapis.com/auth/calendar.settings.readonly');
     provider.addScope('https://www.googleapis.com/auth/calendar.events.readonly');
     provider.addScope('https://www.googleapis.com/auth/calendar.readonly');
     signInWithPopup(authentication, provider)
     .then((res: any) => {
       const { displayName, email, photoUrl, oauthAccessToken, refreshToken } = res._tokenResponse;
+
+      // save user information into database
       getDoc(doc(db, "users", email))
         .then((userData: any) => {
           const friends = userData.data() === undefined ? [] : userData.data().friends;
@@ -26,6 +30,8 @@ const LoginPage = () => {
             refreshToken: refreshToken,
             friends: friends,
           })
+          // set the state of the app to include the user information
+          setUser({ displayName, email, photoUrl, oauthAccessToken, refreshToken, friends });
         })
     })
     .catch(err => {
