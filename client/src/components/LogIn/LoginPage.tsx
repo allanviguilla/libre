1b8f265d954165c2b3c4b5ba2fada536786df7ee
Nick <nicolastiennguyen@gmail.com>
2022-10-03 12:27:33 -0700

    feat: moved everything into loginpage.tsx

--- client/src/components/LogIn/LoginPage.tsx
+++ client/src/components/LogIn/LoginPage.tsx
@@ -1,5 +1,4 @@
-import exp from "constants";
-import React, { useState } from 'react';
+import React from 'react';
 import GoogleButton from 'react-google-button';
 import { authentication, db } from '../../../../configs/config';
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
@@ -10,7 +9,7 @@ const LoginPage = () => {
     const provider = new GoogleAuthProvider();
     provider.addScope('https://www.googleapis.com/auth/calendar.events');
     provider.addScope('https://www.googleapis.com/auth/calendar');
-    provider.addScope('https://www.googleapis.com/auth/calendar.settings.readonly');
+    provider.addScope('https://www.googleapis.com/auth/ƒcalendar.settings.readonly');
     provider.addScope('https://www.googleapis.com/auth/calendar.events.readonly');
     provider.addScope('https://www.googleapis.com/auth/calendar.readonly');
     signInWithPopup(authentication, provider)
@@ -18,7 +17,7 @@ const LoginPage = () => {
       const { displayName, email, photoUrl, oauthAccessToken, refreshToken } = res._tokenResponse;
       getDoc(doc(db, "users", email))
         .then((userData: any) => {
-          const friends = (userData.data() === undefined) ? [] : userData.data().friends;
+          const friends = userData.data() === undefined ? [] : userData.data().friends;
           setDoc(doc(db, "users", email), {
             displayName: displayName,
             email: email,
