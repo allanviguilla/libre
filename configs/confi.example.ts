bc66878d8145f6a0a7b66ab03a4c3ae08a0aa188
jamesEmerson112 <36806380+jamesEmerson112@users.noreply.github.com>
2022-10-03 19:37:59 -0700

    Create confi.example.ts
    Duplicate a template for config

---
+++ configs/confi.example.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  // Including the API Key is not a security risk per Google Cloud.
+  // https://stackoverflow.com/questions/37482366/is-it-safe-to-expose-firebase-apikey-to-the-public
+  apiKey: "AIzaSyAuze6v4dAbSu-xrHeKXMU8w3iaWT6sOfw",
+  authDomain: "libre-4dfda.firebaseapp.com",
+  projectId: "libre-4dfda",
+  storageBucket: "libre-4dfda.appspot.com",
+  messagingSenderId: "645278607294",
+  appId: "1:645278607294:web:75a21135e14b91229a7bef",
+  measurementId: "G-4EXC8ENWYY"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const authentication = getAuth(app);
