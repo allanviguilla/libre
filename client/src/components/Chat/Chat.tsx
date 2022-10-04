9e73f81ef48edded5d97c4b5c69e8b36e2382713
jamesEmerson112 <36806380+jamesEmerson112@users.noreply.github.com>
2022-10-04 13:56:01 -0700

    Update Chat.tsx
    Remove unnecessary comments

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -11,31 +11,8 @@ import {config} from '../../../../configs/config';
 
 // import { doc, setDoc } from "firebase/firestore";
 
-// ==============================================
-// INITIALIZE FIREBASE SDK from doc
-// Import the functions you need from the SDKs you need
-
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBzC-epjFPwzW88k_jzeiJrgtiG680xj80",
-//   authDomain: "chat-demo-firebase-40936.firebaseapp.com",
-//   projectId: "chat-demo-firebase-40936",
-//   storageBucket: "chat-demo-firebase-40936.appspot.com",
-//   messagingSenderId: "724324090088",
-//   appId: "1:724324090088:web:201e4a1692d8c28c44495d",
-//   measurementId: "G-FDWEL1E98M"
-// };
-
 // // Initialize Firebase
 firebase.initializeApp(config);
-// // const analytics = firebase.analytics();
-// const auth = firebase.auth();
 
 const firestore = firebase.firestore();
 
