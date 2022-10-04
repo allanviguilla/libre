ae51cc094cd05e90bf4d3bcb463041f269bfcd31
Kathryn Gao <kathryn.kuroko@gmail.com>
2022-10-03 18:35:04 -0700

    modal

--- client/src/components/HomePage/Calendar/Calendar.tsx
+++ client/src/components/HomePage/Calendar/Calendar.tsx
@@ -6,15 +6,46 @@ import { EventApi, DateSelectArg, EventClickArg, EventContentArg, formatDate } f
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
-import { Tooltip } from "bootstrap";
+import { Button, Tooltip } from "bootstrap";
 import moment from "moment";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  FormControl,
+  FormLabel,
+  Input
+} from '@chakra-ui/react';
 
-import styles from './Calendar.module.css'
+import { useDisclosure } from '@chakra-ui/react'
+
+import styles from './Calendar.module.css';
+
 
 const Calendar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
   return (
     <div className={styles.calendar} id="calendar">
       <h2>CALENDAR HERE</h2>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Create New Event</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <p>HELLO THIS IS EVENT FORM</p>
+            <form>
+              <label>Event name:<input type="text" name="eventName" required></input></label>
+            </form>
+          </ModalBody>
+          <button onClick={onClose}>Submit</button>
+        </ModalContent>
+      </Modal>
       <div className={styles.calendarMain}>
         <FullCalendar
           plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
@@ -32,6 +63,7 @@ const Calendar = () => {
             { title: 'event 1', date: '2022-10-01' },
             { title: 'event 2', date: '2022-10-02' }
           ]}
+          select={onOpen}
           />
       </div>
     </div>
@@ -39,3 +71,5 @@ const Calendar = () => {
 }
 
 export default Calendar;
+
+
