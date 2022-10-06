9e16e65318710647bb3fe59f959fc1c63b1a093e
Kathryn Gao <kathryn.kuroko@gmail.com>
2022-10-05 19:36:23 -0700

    deleted dummy data

--- client/src/components/HomePage/SideBar/FriendsList/FriendsList.tsx
+++ client/src/components/HomePage/SideBar/FriendsList/FriendsList.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react'
 import { BsSearch } from 'react-icons/bs';
 import { HStack, VStack } from '@chakra-ui/react'
 import styles from './../Sidebar.module.css'
-import { friendsReqResponse } from '../dummyData';
 import FriendEntry from './FriendEntry';
 import { connect } from 'react-redux';
 import { doc, setDoc, getDoc } from "firebase/firestore";
