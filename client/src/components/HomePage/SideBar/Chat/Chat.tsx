e54248fbb7199a523906c4bc0f00ded38c817cf2
jamesEmerson112 <36806380+jamesEmerson112@users.noreply.github.com>
2022-10-07 00:36:05 -0700

    Update Chat.tsx - continuously update the chat messages

--- client/src/components/HomePage/SideBar/Chat/Chat.tsx
+++ client/src/components/HomePage/SideBar/Chat/Chat.tsx
@@ -36,29 +36,32 @@ function ChatRoom(props) {
   // collect the data
   const [formValue, setFormValue] = useState('');
 
+  let chatRef = firebase.database().ref('chats/' + identifier).limitToLast(10);
+  let chatData = null;
+
   useEffect(() => {
     // 1. look up chat history for the DM between two friends
     getDoc(doc(db, "chats", identifier))
-    .then((chatData: any) => {
-      // 2. update the current chat
-      const chatHistory = chatData.data() === undefined ? [] : chatData.data().chatHistory;
-      setMessages(chatHistory);
-      const members = chatData.data() === undefined ? [] : [currUser.email, friend.email ].sort();
-      setDoc(doc(db, "chats", identifier), {
-        // members
-        members,
-        chatHistory,
+      .then((chatData: any) => {
+        // 2. update the current chat
+        const chatHistory = chatData.data() === undefined ? [] : chatData.data().chatHistory;
+        setMessages(chatHistory);
+        const members = chatData.data() === undefined ? [] : [currUser.email, friend.email ].sort();
+        setDoc(doc(db, "chats", identifier), {
+          // members
+          members,
+          chatHistory,
+        })
       })
-    })
-    // 3. create a new chat
-    .catch(() => {
-      const members = [currUser.email, friend.email ].sort();
-      setDoc(doc(db, "chats", identifier), {
-        // members
-        members,
+      // 3. create a new chat
+      .catch(() => {
+        const members = [currUser.email, friend.email ].sort();
+        setDoc(doc(db, "chats", identifier), {
+          // members
+          members,
+        })
       })
-    })
-  }, [])
+  }, [messages]);
 
   // sendMessage retrieve the data of a specific chat room
   // then replace it with the new data (new message stored in event)
@@ -68,27 +71,27 @@ function ChatRoom(props) {
     e.preventDefault();
     // get the chat identifier from the Chat component state
     getDoc(doc(db, "chats", identifier))
-    .then((chatData: any) => {
-      // get the document that the has the chat history between two friends
-      chatData = chatData.data();
-      // add message to chat history
-      const timeStamp = getTimeStamp();
-      const text = e.target[0].value;
-      const messageObject = {
-        createdAt: timeStamp,
-        text,
-        email: currUser.email,
-      };
-      const chatHistory = chatData === undefined ? [] : chatData.chatHistory;
-      chatHistory.push(messageObject);
-      setMessages(chatHistory);
-      const members = chatData === undefined ? [] : [currUser.email, friend.email ].sort();
-      // overwrite the existing document with the new chat history object
-      setDoc(doc(db, "chats", identifier), {
-        members,
-        chatHistory,
+      .then((chatData: any) => {
+        // get the document that the has the chat history between two friends
+        chatData = chatData.data();
+        // add message to chat history
+        const timeStamp = getTimeStamp();
+        const text = e.target[0].value;
+        const messageObject = {
+          createdAt: timeStamp,
+          text,
+          email: currUser.email,
+        };
+        const chatHistory = chatData === undefined ? [] : chatData.chatHistory;
+        chatHistory.push(messageObject);
+        setMessages(chatHistory);
+        const members = chatData === undefined ? [] : [currUser.email, friend.email ].sort();
+        // overwrite the existing document with the new chat history object
+        setDoc(doc(db, "chats", identifier), {
+          members,
+          chatHistory,
+        })
       })
-    })
   }
 
   return(
